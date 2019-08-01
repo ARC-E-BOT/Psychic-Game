06cbdfea790134a2f736bfa60837a951316d14f9
Adam Cooper <frankidyfrank3@gmail.com>
2019-07-31 21:18:15 -0500

    added: winner popup at the bottom of the score box styled: the popup at the bottom of the score box

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -17,10 +17,11 @@ const scoreBox = document.getElementById("score-box");
 //starting the score box when the page loads
 updateScores();
 
-//when a key is let go of 
+//when a key is let go of check for winning key or an error 
 document.onkeyup = function(event){
     let key = event.key.toLocaleLowerCase();
     errorMsg = "";
+    console.log(currentCompLetter);
     if(!user.guesses.includes(key) && letters.includes(key)){
         if (key === currentCompLetter){
             winningLetter();
@@ -33,7 +34,7 @@ document.onkeyup = function(event){
             if (key === " ") key = "space";
             errorMsg = `<a class="error-message">ERROR: you have guessed an incorrect key: ${key}</a>`;
         } else {
-            errorMsg = `<a class="error-message">ERROR: you have already guessed ${key}</a>`;
+            errorMsg = `<a class="error-message">ERROR: you have already guessed: ${key}</a>`;
         }
     }
     updateScores();
@@ -49,11 +50,13 @@ function triesTools(){
     }
 }
 
+//handles the winning letter 
 function winningLetter(){
     user.wins++;
     user.remainingTries = 12;
     user.guesses = [];
     currentCompLetter = letters[Math.floor(Math.random() * letters.length)];
+    errorMsg = `<a class="winning-message">WINNER!</a>`;
 }
 
 //updates the scores on the page
